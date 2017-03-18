be22069e1dd9c2a2a895e9510549d49873778c9e
Marcin Wachulski <marcin.wachulski@gmail.com>
2017-03-18 20:35:38 +0100

    #1 Single param read. Green

--- MarWac.Merlin/YamlConfigurationSourceDriver.cs
+++ MarWac.Merlin/YamlConfigurationSourceDriver.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using YamlDotNet.RepresentationModel;
 
 namespace MarWac.Merlin
 {
@@ -6,7 +9,30 @@ namespace MarWac.Merlin
     {
         public Configuration Read(Stream source)
         {
-            return new Configuration();
+            var configuration = new Configuration();
+            
+            using (var reader = new StreamReader(source))
+            {
+                var yaml = new YamlStream();
+                yaml.Load(reader);
+
+                var root = yaml.Documents?.FirstOrDefault()?.RootNode as YamlMappingNode;
+                var parameters = (root?.Children[new YamlScalarNode("parameters")] as YamlSequenceNode)?
+                                       .Children.OfType<YamlMappingNode>();
+
+                if (parameters != null)
+                {
+                    foreach (YamlMappingNode parameter in parameters)
+                    {
+                        KeyValuePair<YamlNode, YamlNode> parameterAssignment = parameter.Children.First();
+                        string parameterName = parameterAssignment.Key.ToString();
+
+                        configuration.Parameters.Add(new ConfigurationParameter(parameterName));
+                    }
+                }
+            }
+
+            return configuration;
         }
     }
 }
