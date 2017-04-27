96d85404dd501cd24541355b015467ac10c62e4a
Marcin Wachulski <marcin.wachulski@gmail.com>
2017-04-27 22:47:53 +0200

    #5 Reading Excel 2003 XML, multiple params read up to first blank. Red

--- MarWac.Merlin.UnitTests/ExcelConfigurationSourceDriver/ReadingTests.cs
+++ MarWac.Merlin.UnitTests/ExcelConfigurationSourceDriver/ReadingTests.cs
@@ -1,4 +1,5 @@
-﻿using MarWac.Merlin.UnitTests.Utils;
+﻿using System.Linq;
+using MarWac.Merlin.UnitTests.Utils;
 using NUnit.Framework;
 
 namespace MarWac.Merlin.UnitTests.ExcelConfigurationSourceDriver
@@ -45,6 +46,44 @@ namespace MarWac.Merlin.UnitTests.ExcelConfigurationSourceDriver
             var ex = Assert.Throws<InvalidExcelConfigurationFormatException>(() => DriverWrapper.ReadExcel(source));
 
             Assert.That(ex.Message, Is.EqualTo("C1 cell should be `Default`"));
+        }
+
+        [Test]
+        public void Read_GivenMultipleParams_ReadsAllParamsUpToFirstBlankRow()
+        {
+            var source = Merlin.ExcelConfigurationSourceDriver.CreateExcelXmlWithRows(@"
+                <Row>
+                  <Cell><Data ss:Type=""String"">Name</Data></Cell>
+                  <Cell><Data ss:Type=""String"">Description</Data></Cell>
+                  <Cell><Data ss:Type=""String"">Default</Data></Cell>
+                </Row>
+                <Row>
+                  <Cell><Data ss:Type=""String"">maxThreads</Data></Cell>
+                  <Cell><Data ss:Type=""String"">Thread limit</Data></Cell>
+                  <Cell><Data ss:Type=""String"">5</Data></Cell>
+                </Row>
+                <Row>
+                  <Cell><Data ss:Type=""String"">apiBaseUri</Data></Cell>
+                  <Cell><Data ss:Type=""String"">Base URI for server REST API</Data></Cell>
+                  <Cell><Data ss:Type=""String"">http://server.com/api</Data></Cell>
+                </Row>
+                <Row ss:Index=""4"">
+                  <Cell><Data ss:Type=""String"">disregardedParam</Data></Cell>
+                  <Cell><Data ss:Type=""String"">This param will not be taken into account</Data></Cell>
+                  <Cell><Data ss:Type=""String""></Data></Cell>
+                </Row>");
+
+            var config = DriverWrapper.ReadExcel(source);
+
+            Assert.That(config.Parameters.Count, Is.EqualTo(2));
+
+            Assert.That(config.Parameters.ElementAt(0).Name, Is.EqualTo("maxThreads"));
+            Assert.That(config.Parameters.ElementAt(0).Description, Is.EqualTo("Thread limit"));
+            Assert.That(config.Parameters.ElementAt(0).DefaultValue, Is.EqualTo("5"));
+
+            Assert.That(config.Parameters.ElementAt(1).Name, Is.EqualTo("apiBaseUri"));
+            Assert.That(config.Parameters.ElementAt(1).Description, Is.EqualTo("Base URI for server REST API"));
+            Assert.That(config.Parameters.ElementAt(1).DefaultValue, Is.EqualTo("http://server.com/api"));
         }
     }
 }
