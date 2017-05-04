bba4bb554dbd44125fad31baa49b5021ccf55923
Marcin Wachulski <marcin.wachulski@gmail.com>
2017-05-04 22:26:32 +0200

    #4 Writing Excel 2003 XML, single param with multiple values. Red

--- MarWac.Merlin.UnitTests/YamlConfigurationSourceDriver/WritingTests.cs
+++ MarWac.Merlin.UnitTests/YamlConfigurationSourceDriver/WritingTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using static MarWac.Merlin.UnitTests.Utils.DriverWrapper;
 
 namespace MarWac.Merlin.UnitTests.YamlConfigurationSourceDriver
@@ -6,8 +7,6 @@ namespace MarWac.Merlin.UnitTests.YamlConfigurationSourceDriver
     [TestFixture]
     public class WritingTests
     {
-        private static readonly string Nl = System.Environment.NewLine;
-
         [Test]
         public void Write_GivenOneParameterWithDefaultValueOnly_WritesCorrectly()
         {
@@ -19,8 +18,49 @@ namespace MarWac.Merlin.UnitTests.YamlConfigurationSourceDriver
 
             var actual = WriteYaml(configuration);
 
-            var expected = "parameters:" + Nl +
-                           "- maxThreads: 5" + Nl;
+            var expected = 
+@"parameters:
+- maxThreads: 5
+";
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Write_GivenOneParameterWithMultipleEnvironmentValues_WritesCorrectly()
+        {
+            var configuration = new Configuration(
+                new[]
+                {
+                    new ConfigurationParameter("maxThreads", "5", new Dictionary<ConfigurableEnvironment, string>
+                    {
+                        {new ConfigurableEnvironment("Local"), "15"},
+                        {new ConfigurableEnvironment("Test"), "25"}
+                    })
+                    {
+                        Description = "Max number of threads"
+                    }
+                },
+                new[]
+                {
+                    new ConfigurableEnvironment("Local"),
+                    new ConfigurableEnvironment("Test")
+                });
+
+            var actual = WriteYaml(configuration);
+
+            var expected =
+@"environments:
+- Local
+- Test
+parameters:
+- maxThreads:
+    description: Max number of threads
+    value:
+    - Local: 15
+    - Test: 25
+    - default: 5
+";
 
             Assert.That(actual, Is.EqualTo(expected));
         }
